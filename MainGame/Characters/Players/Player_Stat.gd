986d2f44490c23ab052d1eee488e4fe69311948a
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2023-01-04 14:52:48 -0500

    add shake camera and shake heart

--- MainGame/Characters/Players/Player_Stat.gd
+++ MainGame/Characters/Players/Player_Stat.gd
@@ -13,3 +13,8 @@ func _process(delta):
 		$VBoxContainer/Red/Num_of_Red.text = '%sx'%[target.PlayerData['Num_of_Red']]
 		$Health.value = (target.health_curr * 100/ target.calculated_health)
 		$Level.value = target.EXP*100/target.EXP_to_next_level
+		
+	if target and float(target.health_curr/ target.calculated_health) < 0.2:
+		$Heart.offset = Vector2(rand_range(-1.0,1.0),rand_range(-1.0,1.0))
+	else:
+		$Heart.offset = Vector2(0,0)
---
+++ MainGame/ViewPortControll/Low_on_Health.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+var target
+
+func _process(delta):
+	if target == null:
+		if get_parent().target != null:
+			target = get_parent().target
+			
+	if target and float(target.health_curr/ target.calculated_health) < 0.2:
+		visible = true
+	else:
+		visible = false
